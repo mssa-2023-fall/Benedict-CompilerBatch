c1f6ba949f0a372c73381a46c7b4f30d179230f9
benazure <matthew.bc.azur3@outlook.com>
2023-09-28 14:12:50 -0700

    recovered stashed Mortgage Calculator

---
+++ BenaryTree/BenaryTree/Class1.cs
@@ -0,0 +1,62 @@
+﻿namespace BenaryTree;
+
+public class Node
+{
+    public int data;
+    public Node left, right;
+
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class BinaryTree
+{
+    public Node root;
+
+    public BinaryTree()
+    {
+        root = null;
+    }
+
+    public void insert(Node root, int key)
+    {
+        if (root == null)
+        {
+            root = new Node(key);
+            return;
+        }
+
+        if (key < root.data)
+        {
+            if (root.left == null)
+            {
+                root.left = new Node(key);
+                return;
+            }
+
+            insert(root.left, key);
+        }
+        else if (key > root.data)
+        {
+            if (root.right == null)
+            {
+                root.right = new Node(key);
+                return;
+            }
+            insert(root.right, key);
+        }
+    }
+
+    public void inorder(Node root)
+    {
+        if (root != null)
+        {
+            inorder(root.left);
+            Console.WriteLine(root.data +" ");
+            inorder(root.right);
+        }
+    }
+}
